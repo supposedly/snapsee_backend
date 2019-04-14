9030d332072760be9f19baed5fe1146e80bc3b31
ht <eltrhn@gmail.com>
2019-04-14 04:19:31 -0700

    Initial commit

---
+++ backend/database.py
@@ -0,0 +1,26 @@
+from backend.flask_sqlalchemy import SQLAlchemy
+
+from backend import app
+
+# good security xd
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+  'postgres+psycopg2://'
+  'kylemumma:password123'
+  '@hackathon-starter-db.c9rs154pvph6.us-west-2.rds.amazonaws.com'
+  ':5432'
+  '/hackathondb'
+)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # suppress deprecation warning
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    uid = db.Column(db.BigInteger, primary_key=True)
+    name = db.Column(db.Text)
+    bitmoji_url = db.Column(db.Text, unique=True)
+    username = db.Column(db.Text, unique=True)
+    image = db.Column(db.LargeBinary)
+
+    def to_dict(self):
+        return {colname: getattr(self, colname) for colname in self.__table__.columns._data}
---
+++ backend/server.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+
+import sqlalchemy
+from sqlalchemy.sql.functions import coalesce
+from flask import request, send_from_directory
+from flask.json import jsonify
+
+from backend import app#, faces
+from backend.database import db, User
+
+def _coalesce_all_columns(model):
+    return {
+      colname: coalesce(get(colname), getattr(model, colname))
+      for colname in model.__table__.columns._data
+    }
+
+
+def _get_all(model, *, filter_none=False):
+    if filter_none:
+        return {colname: get(colname) for colname in model.__table__.columns._data if get(colname) is not None}
+    return {colname: get(colname) for colname in model.__table__.columns._data}
+
+
+def get(key):
+    if key in request.form.files:
+        return request.form.files.get(key).read()
+    return request.values.get(key)
+
+@app.route('/')
+def hello_world():
+    return 'hello world', 200
+
+
+@app.route('/user/add', methods=['POST'])
+def add_user():
+    if not get('username'):
+        return 'Must provide username', 400
+    try:
+        db.session.add(**_get_all(User))
+    except sqlalchemy.exc.IntegrityError as e:
+        db.session.rollback()
+        return str(e), 400  # XXX: 409?
+    db.session.commit()
+    return '', 204
+
+
+@app.route('/user/update', methods=['POST'])
+def edit_user():
+    if not get('username'):
+        return 'Must provide username', 400
+    try:
+        User.query.filter_by(
+          username=get('username')
+        ).update(**_coalesce_all_columns(User))
+    except sqlalchemy.exc.IntegrityError as e:
+        db.session.rollback()
+        return str(e), 400
+    db.session.commit()
+    return '', 204
+
+
+@app.route('/user/get/', methods=['GET'])
+def user_get():
+    try:
+        results = User.query.filter_by(
+          **_get_all(User, filter_none=True)
+        ).all()
+    except Exception as e:
+        return str(e), 400
+    return jsonify([i.to_dict() for i in results]), 200
+
+
+@app.route('/user/get/<colname>', methods=['GET'])
+def user_get_column(colname):
+    try:
+        results = [{colname: getattr(i, colname)} for i in User.query.filter_by(**_get_all(User, filter_none=True)).all()]
+    except Exception as e:
+        return str(e), 400
+    return jsonify(results), 200
+
+
+@app.route('/image/match', methods=['POST'])
+def match_image():
+    return 'Not available', 404
+    match_found = faces.match(get('image'))
